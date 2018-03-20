76f932ddd1e12f23598b39354cf02f87b0b62b51
Fernando Cagua <efcaguab@gmail.com>
2018-03-21 12:44:02 +1300

    Update mac-os.sh

--- mac-os.sh
+++ mac-os.sh
@@ -1,3 +1,34 @@
-xcode-select --install
+# install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew doctor
+brew prune
+
+# install useful unix stuff
+brew install git
+brew install htop
+
+# install cask
 brew tap caskroom/cask
+
+# requisites of R and several popular packages
+xcode-select --install
+brew cask install java
+brew install openssl libssh2 poppler libsvg curl libxml12 gdal geos boost
+
+# install R
+brew install r --with-java --with-openblas
+brew cask install rstudio
+
+# install latex
+brew cask install mactex
+
+# install python
+brew install python
+pip install --upgrade pip
+pip install jupyter
+
+# install other tools
+brew cask visual-studio-code
+brew cask slack
+brew cask toggl
