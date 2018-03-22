f6fda9b5025990674be38b2f9d6cbb1e3fd4cd0b
Fernando Cagua <efcaguab@gmail.com>
2018-03-22 19:24:37 +1300

    Update mac-os.sh

--- mac-os.sh
+++ mac-os.sh
@@ -3,6 +3,7 @@ xcode-select --install
 # install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 echo 'export PATH=/usr/local/bin:/usr/local/sbin:$PATH' >>~/.profile
+echo 'export PATH=/usr/local/bin:/usr/local/sbin:$PATH' >>~/.bash_profile
 echo 'export PATH=/usr/local/bin:$PATH' >>~/.profile
 echo 'export PATH=/usr/local/bin:$PATH' >>~/.bash_profile
 source ~/.bash_profile
@@ -12,5 +13,14 @@ brew bundle
 
 # python stuff
 pip3 install --upgrade pip
-pip3 -install -user pipenv
+pip3 install -user pipenv
 echo "export PATH=$PATH:$(echo -n $(python3 -m site --user-base) ; echo /bin)" >> ~/.profile
+pip3 install -user virtualenv
+# data science packages
+pip3 install jupyter
+pip3 install matplotlib
+pip3 install scipy
+pip3 install pandas
+pip3 install numpy
+pip3 install seaborn
+pip3 install pymc3
